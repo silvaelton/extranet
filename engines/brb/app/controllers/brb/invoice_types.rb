543c174edc28fcff76e453a64257b6f9c443cf13
cleuber <cleubercanamari11@gmail.com>
2019-01-28 16:27:36 -0200

    engine brb - categury, invoice_type

---
+++ engines/brb/app/controllers/brb/invoice_types.rb
@@ -0,0 +1,45 @@
+require_dependency 'brb/application_controller'
+
+module Brb
+	class InvoiceTypesController < ApplicationController
+		before_action :set_invoice_types
+		before_action :set_invoice_type, only: %i[edit update destroy]
+
+		def index; end
+
+		def new
+			@invoice_type = Brb::InvoiceType.new
+		end
+
+		def create
+			@invoice_type = Brb::InvoiceType.new(set_params)
+			@invoice_type.save
+		end
+
+		def edit; end
+
+		def update
+			@invoice_type.update(set_params)
+			@invoice_type.save
+		end
+
+		def destroy
+			@invoice_type.destroy
+		end
+
+		private
+
+		def set_params
+			params.require(:invoice_type).permit(:name, :status, :invoice, :accout)
+		end
+
+		def set_invoice_types
+			@invoice_types = Brb::InvoiceType.all
+		end
+
+		def set_invoice_type
+			@invoice_type = Brb::InvoiceType.find(params[:id])
+		end
+
+	end
+end
