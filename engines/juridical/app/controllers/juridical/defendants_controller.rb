5d1e90924b0c5e3d41f701e0b9552c256f7dac08
Igor Marques <igormarjes@gmail.com>
2019-02-26 11:29:20 -0300

    Finalizacao engine juridico

--- engines/juridical/app/controllers/juridical/defendants_controller.rb
+++ engines/juridical/app/controllers/juridical/defendants_controller.rb
@@ -3,10 +3,7 @@ require_dependency 'juridical/application_controller'
 module Juridical
   class DefendantsController < ApplicationController
     before_action :set_legal_advice
-    before_action :set_defendants, only: [:index, :create, :destroy, :update]
     before_action :set_defendant, only: [:edit, :destroy, :update]
-
-    def index; end
 
     def new
       @defendant = @legal_advice.defendants.new
@@ -14,9 +11,8 @@ module Juridical
 
     def create
       @defendant = @legal_advice.defendants.new(defendant_params)
-      @defendant.staff_id = current_user.id
+      @defendant.user_id = current_user.id
       @defendant.save
-      @legal_advices = Juridical::LegalAdvice.order(created_at: :desc).includes([:lawsuit, :complements]).paginate(:page => params[:page], :per_page => 20)
     end
 
     def edit; end
@@ -33,10 +29,6 @@ module Juridical
 
     def defendant_params
       params.require(:defendant).permit(:name)
-    end
-
-    def set_defendants
-      @defendants = @legal_advice.defendants.all
     end
 
     def set_defendant
--- engines/juridical/app/models/juridical/complement.rb
+++ engines/juridical/app/models/juridical/complement.rb
@@ -4,18 +4,21 @@ module Juridical
   class Complement < Support::Juridical::Complement
     attr_accessor :instancy
 
-    belongs_to :document_type,        required: false, class_name: "Support::Protocol::DocumentType"
+    belongs_to :document_type,        required: false, class_name: "Protocol::DocumentType"
     belongs_to :lawsuit,              required: false
     belongs_to :instancy_place,       required: false
-    #belongs_to :responsible_lawyer,   required: false,-> {where(sector_current_id: 5)}, foreign_key: "responsible_lawyer_id", class_name: "Person::Staff"
+
+    # sector_id: 5 (Proju)
+    belongs_to :responsible_lawyer, -> {where(sector_id: 5)},   required: false, class_name: "Pivotal::User", foreign_key: "responsible_lawyer_id"
+
     belongs_to :advice_type,          required: false
     belongs_to :complement_father_id, required: false, foreign_key: "id", class_name: "Juridical::Complement"
     belongs_to :legal_advice,         required: false
-    belongs_to :staff,                required: false, class_name: "Person::Staff"
+    belongs_to :user,                 required: false, class_name: "Pivotal::User"
 
     mount_uploader :attachment, Juridical::AttachmentUploader
 
-    validates :distribution_date, :complement ,presence: true
+    validates :complement ,presence: true
 
     scope :by_year,        ->(year)        { where("date_part('year', created_at) = ?", year) }
     scope :by_month,       ->(month)       { where("date_part('month', created_at) = ?", month) }
@@ -23,5 +26,14 @@ module Juridical
     scope :by_responsible, ->(responsible) { where(responsible_lawyer_id: responsible) }
     scope :by_status,      ->(status)      { where(status: status) }
     scope :by_date,        ->(date)        { where("created_at::date = ?", Date.parse(date)) }
+
+    after_save :send_email
+
+    def send_email
+      begin
+       Juridical::InformationMailer.open_complement(self).deliver_now! if self.responsible_lawyer_id.present?
+      rescue
+      end
+    end
   end
 end
