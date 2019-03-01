86cca94189537d2ec3d643d1501648c7588ff92c
Elton Silva <elton.chrls@gmail.com>
2019-03-01 10:27:20 -0300

    Adicionando generate

--- lib/generators/crud_generator.rb
+++ lib/generators/crud_generator.rb
@@ -1,94 +1,95 @@
 class CrudGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  argument :model, type: :string
-  argument :type,  type: :string
   
+  argument :model,      type: :string
+  argument :type,       type: :string
+  argument :attributes, type: :array
+  
+  attr_accessor :engine, :model_name
+
   def generate_init
+    @model   = @model.underscore.split('/')
+    
+    @engine_name = @model[0] 
+    @model_name  = @model[1]
+    
+    generate_files!
+
     case type
+    when 'show'
+      generate_show
     when 'show.js'
-      generate_with_show_modal
-    when 'show.html'
-      generate_without_show_modal
-    else
+      generate_show_modal
+    when 'no_show'
       generate_without_show
     end
   end
 
   private
 
-  def generate_without_show_modal
-    files = %w(
-                index.html.haml
-                _index.html.haml
-                _index_sidebar.html.haml
-                index.js.haml
-                show.html.haml
-                _show_modal.html.haml
-                _show.html.haml
-                _show_sidebar.html.haml
-                _new.html.haml
-                _edit.html.haml
-                update.js.haml
-                create.js.haml
-                destroy.js.haml
-                _save.js.haml
-              )
+  def generate_files!
+    view_files = %w(
+      _new.html.haml
+      new.js.haml
+      edit.js.haml
+      _edit.html.haml
+      create.js.haml
+      destroy.js.haml
+      update.js.haml
+      _save.js.haml
+      index.js.haml
+      _index.html.haml
+      _index_sidebar.html.haml
+      _form.html.haml
+    )
 
-    generate!(files, model)    
+    view_files.each do |file|
+      copy(view_files, @engine_name, @model_name, 'views')
+    end
+
+    copy(['controller.template'], @engine_name, @model_name, 'controllers')
+    copy(['model.template'], @engine_name, @model_name, 'models') 
   end
 
-  def generate_without_show
-    files = %w(
-                index.html.haml
-                _index.html.haml
-                _index_sidebar.html.haml
-                index.js.haml
-                _new.html.haml
-                _edit.html.haml
-                update.js.haml
-                create.js.haml
-                destroy.js.haml
-                _save.js.haml
-              )
+  def generate_show_modal
+    
+    view_files = %w(
+      show.js.haml
+      _show_modal.html.haml
+    )
+    
+    view_files.each do |file|
+      copy(view_files, @engine_name, @model_name, 'views')
+    end
+  end
 
-    generate!(files, model)
-  end 
+  def generate_show
+    view_files = %w(
+      show.html.haml
+      _show.html.haml
+      _show_sidebar.html.haml
+    )
 
-  def generate_with_show_modal
-    files = %w(
-                index.html.haml
-                _index.html.haml
-                _index_sidebar.html.haml
-                index.js.haml
-                show.html.haml
-                show.js.haml
-                _show_modal.html.haml
-                _show.html.haml
-                _show_sidebar.html.haml
-                _new.html.haml
-                _edit.html.haml
-                update.js.haml
-                create.js.haml
-                destroy.js.haml
-                _save.js.haml
-              )
-
-    generate!(files, model)
+    view_files.each do |file|
+      copy(view_files, @engine_name, @model_name, 'views')
+    end
   end
 
 
-  def generate!(files_path, model)
+  def copy(files_path, engine, model, folder)
+
     files_path.each do |file|
-      copy_file "views/#{file}", "engines/#{engine}/app/#{engine}/views/#{model.underscore}/#{file}"
+      case folder
+      when 'views'
+        template "#{folder}/#{file}", "engines/#{engine}/app/#{folder}/#{engine}/#{model.underscore.pluralize}/#{file}"
+      when 'controllers'
+        template  "#{folder}/#{file}", "engines/#{engine}/app/#{folder}/#{engine}/#{model.underscore.pluralize}_controller.rb"
+      when 'models'
+        template  "#{folder}/#{file}", "engines/#{engine}/app/#{folder}/#{engine}/#{model.underscore}.rb"
+      else
+      end
     end
 
   end
 
-  def generate_controller
-    copy_file "controller/#{file}", "engines/#{engine}/app/#{engine}/controllers/#{model.underscore}/#{file}"
-  end
-  
-  def generate_model
-    copy_file "model/#{file}", "engines/#{engine}/app/#{engine}/models/#{model.underscore}/#{file}"
-  end
 end
