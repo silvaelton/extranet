108436e1459ddf91ec367dc1fbd030452207ee5c
Igor Marques <igormarjes@gmail.com>
2019-04-12 17:26:21 -0300

    Atualizar

--- engines/address/app/models/address/unit.rb
+++ engines/address/app/models/address/unit.rb
@@ -3,5 +3,25 @@ require_dependency 'support/address/unit'
 module Address
   class Unit < Support::Address::Unit
     belongs_to :city, required: false, class_name: 'Support::Common::City'
+
+    scope :by_city,  ->(city) { where(city_id: city) }
+    scope :by_burgh, ->(burgh) { where(burgh: burgh) }
+    scope :by_group, ->(group) { where(group: group) }
+    scope :by_block, ->(block) { where(block: block) }
+    scope :by_unit,  ->(unit)    { where(unit: unit) }
+
+    scope :by_situation, ->(situation) {
+      where(id: Support::Address::UnitSituation.select(:unit_id).where(created_at: Support::Address::UnitSituation
+                                            .select("MAX(created_at)")
+                                            .group(:unit_id), situation_type_id: situation))
+
+     }
+
+    scope :by_registry, -> (registry) {
+      where(id: Support::Address::UnitRegistry.select(:unit_id).where(created_at: Support::Address::UnitRegistry
+                                            .select("MAX(created_at)")
+                                            .group(:unit_id), registry_type_id: registry))
+
+     }
   end
 end
