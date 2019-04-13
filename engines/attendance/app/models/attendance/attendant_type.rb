7855f5d950e847f60352d8632371019075685e32
cleuber <cleubercanamari11@gmail.com>
2019-04-13 09:18:20 -0300

    Atualização de inscrição eventual

--- engines/attendance/app/models/attendance/attendant_type.rb
+++ engines/attendance/app/models/attendance/attendant_type.rb
@@ -3,7 +3,7 @@ require_dependency 'support/attendance/attendant_type'
 module Attendance
   class AttendantType < Support::Attendance::AttendantType
 
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: { scope: :deleted}
 
     scope :by_name,       ->(name) { where("name ilike '%#{name}%'") }
     scope :by_status,      ->(status) { where(status: status) }
