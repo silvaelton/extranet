0cf64a634f6ba7cea87aa74759059ab8987e14f0
cleuber <cleubercanamari11@gmail.com>
2019-04-15 00:50:45 -0300

    Atualização address image

--- engines/address/app/models/address/unit_image.rb
+++ engines/address/app/models/address/unit_image.rb
@@ -2,7 +2,7 @@ require_dependency 'support/address/unit_image'
 
 module Address
   class UnitImage < Support::Address::UnitImage
-    
+
     validates :image, presence: true
 
     mount_uploader :image, Support::ImageUploader
